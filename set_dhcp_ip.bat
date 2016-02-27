cd7e167bb89a7465fab257a61a71eca17224ba02
XLILT <xiaolongma002@163.com>
2016-02-27 09:37:52 +0800

    Update set_dhcp_ip.bat
    bug fix

--- set_dhcp_ip.bat
+++ set_dhcp_ip.bat
@@ -1,11 +1,11 @@
 @echo off 
 echo 正在配置动态ip,请稍等... 
 
-set name="本地连接"
+set name="以太网"
 
 echo 正在配置%name%,请稍等... 
 echo 正在配置动态DNS...
-netsh interface ip set dns name="本地连接" source=dhcp
+netsh interface ip set dns name=%name% source=dhcp
 
 echo 正在配置动态ip...
 netsh interface ip set address name=%name% source=dhcp
